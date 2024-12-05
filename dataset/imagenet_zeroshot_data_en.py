fa9ab58204a1fe32ae25263ece3ecdff55b582af
“Yangless” <bielangmeng@163.com>
2024-12-05 21:58:40 +0800

    Add mscoco_template

--- dataset/imagenet_zeroshot_data_en.py
+++ dataset/imagenet_zeroshot_data_en.py
@@ -551,3 +551,30 @@ _template = [
     'a centered satellite photo of the {}.',
 ]
 data_template['sat'] = [eval("lambda c:f'{}'".format(i).replace('{}','{c}')) for i in _template]
+# Define MSCOCO classnames and templates
+mscoco_classnames = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+    "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball",
+    "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+    "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+    "chair", "couch", "potted plant", "bed", "dining table", "toilet", "TV", "laptop",
+    "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster",
+    "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier",
+    "toothbrush"
+]
+
+mscoco_template = [
+    lambda c: f'a photo of a {c}.',
+    lambda c: f'a bad photo of the {c}.',
+    lambda c: f'a photo of many {c}.',
+    lambda c: f'a photo of several {c} in the scene.',
+    lambda c: f'a realistic depiction of a {c}.'
+]
+
+# Integrate these into the data_classnames and data_template dictionaries
+data_classnames['mscoco'] = mscoco_classnames
+data_template['mscoco'] = mscoco_template
+
